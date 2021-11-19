4cffcefe456e79bad38034d0ac01c5548321c28e
IvanDelfin <75266088+IvanDelfin@users.noreply.github.com>
2021-11-19 10:07:29 -0600

    Creacion del estacionamientotest
    subí el codigo del estacionamiento

---
+++ Programa/EstacionamientoTest.py
@@ -0,0 +1,138 @@
+# Model design
+import agentpy as ap
+import networkx as nx
+import random
+import time
+
+# Visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+import IPython
+
+class Person(ap.Agent):
+
+    def setup(self):
+        """ Initialize a new variable at agent creation. """
+        self.condition = 0  # Buscando lugar = 0, Encontró lugar = 1, esta en lugar = 2 De salida = 3
+        self.LugaresDisponibles = self.p.lugares_disponibles
+
+    def being_sick(self):
+        """ Spread disease to peers in the network. """
+        rng = self.model.random
+        for n in self.network.neighbors(self):
+            if n.condition == 0 and self.LugaresDisponibles != 0 and self.p.chance_de_encontrar_lugar > rng.random():
+                n.condition = 1  # Encontró lugar
+
+            if self.p.A_Ver_Si_No_Se_Pierde > rng.random():
+                self.condition = 2  # Recover from infection
+                self.LugaresDisponibles = self.LugaresDisponibles - 1
+                
+            if self.p.ya_se_va > rng.random():
+                self.condition = 3 # ya se va de salida
+                self.LugaresDisponibles = self.LugaresDisponibles + 1
+            
+class VirusModel(ap.Model):
+
+    def setup(self):
+        """ Initialize the agents and network of the model. """
+
+        # Prepare a small-world network
+        graph = nx.watts_strogatz_graph(
+            self.p.population,
+            self.p.number_of_neighbors,
+            self.p.network_randomness)
+
+        # Create agents and network
+        self.agents = ap.AgentList(self, self.p.population, Person)
+        self.network = self.agents.network = ap.Network(self, graph)
+        self.network.add_agents(self.agents, self.network.nodes)
+
+        # Infect a random share of the population
+        I0 = int(self.p.initial_infection_share * self.p.population)
+        self.agents.random(I0).condition = 1
+
+    def update(self):
+        """ Record variables after setup and each step. """
+
+        # Record share of agents with each condition
+        for i, c in enumerate(('S', 'I', 'R', 'D')):
+            n_agents = len(self.agents.select(self.agents.condition == i))
+            self[c] = n_agents / self.p.population
+            self.record(c)
+
+        # Stop simulation if disease is gone
+        if self.I == 0:
+            self.stop()
+
+    def step(self):
+        """ Define the models' events per simulation step. """
+
+        # Call 'being_sick' for infected agents
+        self.agents.select(self.agents.condition == 1).being_sick()
+
+    def end(self):
+        """ Record evaluation measures at the end of the simulation. """
+
+        # Record final evaluation measures
+        self.report('Total share infected', self.I + self.R)
+        self.report('Peak share infected', max(self.log['I']))
+        
+parameters = {
+    'population': 100,
+    'chance_de_encontrar_lugar': 0.5,
+    'A_Ver_Si_No_Se_Pierde': 0.8,
+    'ya_se_va': 0.6,
+    'initial_infection_share': 0.1,
+    'number_of_neighbors': 2,
+    'network_randomness': 0.5,
+    'lugares_disponibles': 80
+}
+
+model = VirusModel(parameters)
+results = model.run()
+
+results
+
+def virus_stackplot(data, ax):
+    """ Stackplot of people's condition over time. """
+    x = data.index.get_level_values('t')
+    y = [data[var] for var in ['I', 'S', 'R', 'D']]
+
+    sns.set()
+    ax.stackplot(x, y, labels=['LEncontado', 'BuscandoL', 'EstáEnL', 'Salida'],
+                 colors = ['r', 'b', 'g', 'yellow'])
+
+    ax.legend()
+    ax.set_xlim(0, max(1, len(x)-1))
+    ax.set_ylim(0, 1)
+    ax.set_xlabel("Time steps")
+    ax.set_ylabel("Percentage of population")
+
+fig, ax = plt.subplots()
+virus_stackplot(results.variables.VirusModel, ax)
+
+def animation_plot(m, axs):
+    ax1, ax2 = axs
+    ax1.set_title("Virus spread")
+    ax2.set_title(f"Share infected: {m.I}")
+
+    # Plot stackplot on first axis
+    virus_stackplot(m.output.variables.VirusModel, ax1)
+
+    # Plot network on second axis
+    color_dict = {0:'b', 1:'r', 2:'g', 3:'yellow'}
+    colors = [color_dict[c] for c in m.agents.condition]
+    nx.draw_circular(m.network.graph, node_color=colors,
+                     node_size=50, ax=ax2)
+
+fig, axs = plt.subplots(1, 2, figsize=(8, 4)) # Prepare figure
+parameters['population'] = 50 # Lower population for better visibility
+animation = ap.animate(VirusModel(parameters), fig, axs, animation_plot)
+
+IPython.display.HTML(animation.to_jshtml())
+
+plt.show()
+
+animation.save('virus_model.gif', writer='imagemagick')
+
+IPython.display.HTML(animation.to_jshtml())
