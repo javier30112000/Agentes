367d262ababda46cad75746de4306820ba4f9bd9
Iván <A01024265@itesm.mx>
2021-11-30 11:15:16 -0600

    V.1.2Sim

---
+++ Simulacion - Copy/Assets/MovimientoPeatonal.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPeatonal : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    void Start(){
+        target = Waypoints.points[0];
+    }
+
+    void Update(){
+        Vector3 direct = target.position - transform.position;
+        transform.Translate(direct.normalized * speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f){
+            GetNextWaypoint();
+        }
+
+
+    }
+
+    void GetNextWaypoint(){
+        if (wavepointIndex >= Waypoints.points.Length - 1){
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+
+}
